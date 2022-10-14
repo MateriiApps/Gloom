41cfbf92b4226ace2e200f1f7838f0f229feb02d
wingio <wingio@users.noreply.github.com>
2022-10-14 14:28:58 -0400

    Improve login page

--- app/src/main/java/com/materiapps/gloom/ui/screens/auth/LandingScreen.kt
+++ app/src/main/java/com/materiapps/gloom/ui/screens/auth/LandingScreen.kt
@@ -1,15 +1,15 @@
 package com.materiapps.gloom.ui.screens.auth
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.koin.getScreenModel
 import com.materiapps.gloom.R
@@ -30,9 +30,33 @@ class LandingScreen : Screen {
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
+                Spacer(Modifier.weight(0.20f))
+                Image(
+                    painter = painterResource(R.drawable.ic_gloom),
+                    contentDescription = "Gloom Icon",
+                    modifier = Modifier.size(300.dp)
+                )
+                Spacer(modifier = Modifier.weight(0.10f))
+                Text(
+                    text = stringResource(R.string.welcome),
+                    style = MaterialTheme.typography.headlineLarge.copy(
+                        fontWeight = FontWeight.Thin
+                    ),
+                    modifier = Modifier.padding(bottom = 24.dp)
+                )
                 Button(onClick = { viewModel.signIn() }) {
-                    Text(stringResource(R.string.sign_in))
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(ButtonDefaults.IconSpacing),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            painterResource(R.drawable.ic_github_24),
+                            contentDescription = "GitHub Logo"
+                        )
+                        Text(stringResource(R.string.sign_in))
+                    }
                 }
+                Spacer(Modifier.weight(0.20f))
             }
         }
     }
