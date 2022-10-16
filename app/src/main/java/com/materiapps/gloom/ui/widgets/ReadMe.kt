3135ffec1926adf70d8c7b6830595f2a85793ac9
wingio <wingio@users.noreply.github.com>
2022-10-16 13:56:33 -0400

    Feat: Collapse README by default

--- app/src/main/java/com/materiapps/gloom/ui/widgets/ReadMe.kt
+++ app/src/main/java/com/materiapps/gloom/ui/widgets/ReadMe.kt
@@ -7,16 +7,21 @@ import android.view.ViewGroup
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ElevatedCard
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDownward
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.web.AccompanistWebViewClient
 import com.google.accompanist.web.WebView
 import com.google.accompanist.web.rememberWebViewStateWithHTMLData
+import com.materiapps.gloom.R
 import com.materiapps.gloom.utils.generateMdHtml
 
 
@@ -24,19 +29,58 @@ import com.materiapps.gloom.utils.generateMdHtml
 fun ReadMeCard(
     text: String
 ) {
+    var isCollapsed by remember {
+        mutableStateOf(true)
+    }
+
     ElevatedCard {
         Box(
-            modifier = Modifier.padding(16.dp)
+            modifier = if (isCollapsed) Modifier.height(160.dp) else Modifier
         ) {
             Markdown(
-                text
+                text,
+                modifier = if (isCollapsed)
+                    Modifier.padding(top = 16.dp, start = 16.dp, end = 16.dp)
+                else
+                    Modifier.padding(16.dp)
             )
+            if (isCollapsed) Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.BottomCenter)
+                    .background(
+                        Brush.verticalGradient(
+                            listOf(
+                                Color.Transparent,
+                                MaterialTheme.colorScheme.surfaceColorAtElevation(1.dp)
+                            )
+                        )
+                    )
+                    .padding(10.dp)
+            ) {
+                FilledTonalButton(
+                    onClick = { isCollapsed = false },
+                    modifier = Modifier.align(Alignment.Center)
+                ) {
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(ButtonDefaults.IconSpacing),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowDownward,
+                            null,
+                            modifier = Modifier.size(17.dp)
+                        )
+                        Text(stringResource(R.string.read_more))
+                    }
+                }
+            }
         }
     }
 }
 
-@SuppressLint("ClickableViewAccessibility")
 @Composable
+@SuppressLint("ClickableViewAccessibility")
 fun Markdown(
     text: String,
     modifier: Modifier = Modifier
@@ -53,7 +97,9 @@ fun Markdown(
 
     WebView(
         state,
-        modifier = Modifier.background(Color.Transparent),
+        modifier = Modifier
+            .background(Color.Transparent)
+            .then(modifier),
         client = client,
         onCreated = {
             it.setBackgroundColor(android.graphics.Color.TRANSPARENT)
