85cd8935129d4ddba4bf62d18d6ab9eecdcf814e
wingio <wingio@users.noreply.github.com>
2022-10-17 12:52:13 -0400

    Update token refreshing

--- app/src/main/java/com/materiapps/gloom/domain/manager/AuthManager.kt
+++ app/src/main/java/com/materiapps/gloom/domain/manager/AuthManager.kt
@@ -1,18 +1,27 @@
 package com.materiapps.gloom.domain.manager
 
 import android.content.Context
+import androidx.core.content.edit
 import com.materiapps.gloom.domain.manager.base.BasePreferenceManager
 import com.materiapps.gloom.domain.repository.GithubAuthRepository
 import com.materiapps.gloom.rest.utils.ifSuccessful
 import com.materiapps.gloom.utils.coroutine
 
-class AuthManager(context: Context, private val authRepo: GithubAuthRepository) :
-    BasePreferenceManager(context.getSharedPreferences("auth", Context.MODE_PRIVATE)) {
+class AuthManager(context: Context, private val authRepo: GithubAuthRepository) {
 
-    var authToken by stringPreference("authToken", "")
-    var refreshToken by stringPreference("refreshToken", "")
+    private val sharedPrefs = context.getSharedPreferences("auth", Context.MODE_PRIVATE)
+    var authToken: String
+        get() = sharedPrefs.getString("authToken", "")!!
+        set(value) = sharedPrefs.edit {
+            putString("authToken", value)
+        }
+    var refreshToken: String
+        get() = sharedPrefs.getString("refreshToken", "")!!
+        set(value) = sharedPrefs.edit {
+            putString("refreshToken", value)
+        }
 
-    val isSignedIn = authToken.isNotEmpty()
+    val isSignedIn get() = authToken.isNotEmpty()
 
     fun refreshAccessToken() {
         coroutine {
@@ -24,4 +33,5 @@ class AuthManager(context: Context, private val authRepo: GithubAuthRepository) 
             }
         }
     }
+
 }
--- app/src/main/java/com/materiapps/gloom/ui/MainActivity.kt
+++ app/src/main/java/com/materiapps/gloom/ui/MainActivity.kt
@@ -9,7 +9,7 @@ import org.koin.android.ext.android.inject
 
 class MainActivity : ComponentActivity() {
 
-    val authManager: AuthManager by inject()
+    private val authManager: AuthManager by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
