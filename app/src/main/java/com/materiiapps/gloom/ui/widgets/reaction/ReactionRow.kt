5cba96f04d39442137bca41fb8167b3d573c3815
wingio <wingio@users.noreply.github.com>
2023-07-02 15:59:42 -0400

    Update reactions list on refresh

--- app/src/main/java/com/materiiapps/gloom/ui/widgets/reaction/ReactionRow.kt
+++ app/src/main/java/com/materiiapps/gloom/ui/widgets/reaction/ReactionRow.kt
@@ -1,5 +1,6 @@
 package com.materiiapps.gloom.ui.widgets.reaction
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
@@ -19,11 +20,14 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -33,6 +37,8 @@ import com.materiiapps.gloom.gql.type.ReactionContent
 import com.materiiapps.gloom.ui.theme.colors
 import com.materiiapps.gloom.utils.Constants
 
+//@SuppressLint("UnrememberedMutableState")
+@SuppressLint("UnrememberedMutableState")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ReactionRow(
@@ -41,12 +47,15 @@ fun ReactionRow(
     forRelease: Boolean = false
 ) {
     val _reactions = remember {
-        mutableStateListOf<Reaction>().apply {
-            addAll(reactions.filter { it.reactors.totalCount > 0 })
-        }
+        mutableStateListOf<Reaction>()
     }
     var opened by remember {
         mutableStateOf(false)
+    }
+
+    LaunchedEffect(reactions) {
+        _reactions.clear()
+        _reactions.addAll(reactions.filter { it.reactors.totalCount > 0 })
     }
 
     fun react(reaction: ReactionContent) {
