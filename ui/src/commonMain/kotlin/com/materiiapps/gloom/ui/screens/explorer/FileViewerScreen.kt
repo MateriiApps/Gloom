1947b6bfe2b3200bd3fdca30cf3204af574b1e38
Wing <44992537+wingio@users.noreply.github.com>
2023-10-08 16:16:24 -0400

    Add error ui to file viewer

--- ui/src/commonMain/kotlin/com/materiiapps/gloom/ui/screens/explorer/FileViewerScreen.kt
+++ ui/src/commonMain/kotlin/com/materiiapps/gloom/ui/screens/explorer/FileViewerScreen.kt
@@ -1,10 +1,17 @@
 package com.materiiapps.gloom.ui.screens.explorer
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Cancel
@@ -12,16 +19,21 @@ import androidx.compose.material.icons.filled.Share
 import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.koin.getScreenModel
 import com.materiiapps.gloom.Res
@@ -65,14 +77,10 @@ class FileViewerScreen(
                     .pullRefresh(pullRefreshState)
             ) {
                 when (viewModel.hasError) {
-                    true -> {
-                        Icon(
-                            Icons.Filled.Cancel,
-                            contentDescription = null,
-                            tint = MaterialTheme.colorScheme.error,
-                            modifier = Modifier.align(Alignment.Center)
-                        )
-                    }
+                    true -> ErrorMessage(
+                        message = stringResource(Res.strings.msg_file_load_error),
+                        onRetryClick = viewModel::getRepoFile
+                    )
 
                     false -> FileContent(file)
                 }
@@ -90,6 +98,43 @@ class FileViewerScreen(
             "PdfFileType" -> Box(Modifier.fillMaxSize())
             "TextFileType" -> TextFileViewer(file.fileType?.onTextFileType!!, file.extension ?: "")
             else -> {}
+        }
+    }
+
+    @Composable
+    private fun BoxScope.ErrorMessage(
+        message: String,
+        onRetryClick: () -> Unit
+    ) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .align(Alignment.Center)
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Cancel,
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.error,
+                modifier = Modifier.size(50.dp)
+            )
+
+            Text(
+                message,
+                style = MaterialTheme.typography.titleMedium,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            FilledTonalButton(
+                onClick = onRetryClick,
+            ) {
+                Text(stringResource(Res.strings.action_try_again))
+            }
         }
     }
 
