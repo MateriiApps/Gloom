35664cb48d44715c2dd1861c8af02417913a7fb5
Wing <44992537+wingio@users.noreply.github.com>
2024-09-18 16:01:39 -0400

    Add support for per-app languages

--- app/android/build.gradle.kts
+++ app/android/build.gradle.kts
@@ -38,6 +38,10 @@ android {
         }
     }
 
+    androidResources {
+        generateLocaleConfig = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
