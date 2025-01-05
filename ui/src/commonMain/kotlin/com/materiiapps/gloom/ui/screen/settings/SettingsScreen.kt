76cf337ad6061642a7b46502883e25f72dc2e097
Wing <44992537+wingio@users.noreply.github.com>
2025-01-04 19:12:17 -0500

    feat: Sign out icon button

--- ui/src/commonMain/kotlin/com/materiiapps/gloom/ui/screen/settings/SettingsScreen.kt
+++ ui/src/commonMain/kotlin/com/materiiapps/gloom/ui/screen/settings/SettingsScreen.kt
@@ -5,12 +5,17 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.Logout
 import androidx.compose.material.icons.outlined.AccountCircle
 import androidx.compose.material.icons.outlined.Code
 import androidx.compose.material.icons.outlined.DesignServices
 import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material.icons.outlined.Palette
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
@@ -25,7 +30,6 @@ import com.materiiapps.gloom.Res
 import com.materiiapps.gloom.ui.component.toolbar.LargeToolbar
 import com.materiiapps.gloom.ui.screen.auth.LandingScreen
 import com.materiiapps.gloom.ui.screen.settings.about.AboutScreen
-import com.materiiapps.gloom.ui.screen.settings.component.SettingsButton
 import com.materiiapps.gloom.ui.screen.settings.component.SettingsCategory
 import com.materiiapps.gloom.ui.screen.settings.component.account.SignOutDialog
 import com.materiiapps.gloom.ui.screen.settings.developer.DeveloperSettingsScreen
@@ -36,16 +40,12 @@ import com.materiiapps.gloom.util.IsDeveloper
 import com.materiiapps.gloom.util.VersionName
 import dev.icerock.moko.resources.compose.stringResource
 
-class SettingsScreen : Screen {
+class SettingsScreen: Screen {
 
     @Composable
-    override fun Content() = Screen()
-
     @OptIn(ExperimentalMaterial3Api::class)
-    @Composable
-    private fun Screen(
-        viewModel: SettingsViewModel = getScreenModel()
-    ) {
+    override fun Content() {
+        val viewModel: SettingsViewModel = getScreenModel()
         val nav = LocalNavigator.currentOrThrow
         val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
 
@@ -62,7 +62,7 @@ class SettingsScreen : Screen {
         }
 
         Scaffold(
-            topBar = { Toolbar(scrollBehavior) },
+            topBar = { Toolbar(scrollBehavior, onSignOutClick = viewModel::openSignOutDialog) },
             modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection)
         ) {
             Column(
@@ -108,10 +108,6 @@ class SettingsScreen : Screen {
                     subtext = "${stringResource(Res.strings.app_name)} v$VersionName",
                     destination = ::AboutScreen
                 )
-
-                SettingsButton(label = stringResource(Res.strings.action_sign_out)) {
-                    viewModel.openSignOutDialog()
-                }
             }
         }
     }
@@ -119,11 +115,23 @@ class SettingsScreen : Screen {
     @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     private fun Toolbar(
-        scrollBehavior: TopAppBarScrollBehavior
+        scrollBehavior: TopAppBarScrollBehavior,
+        onSignOutClick: () -> Unit
     ) {
         LargeToolbar(
             title = stringResource(Res.strings.navigation_settings),
-            scrollBehavior = scrollBehavior
+            scrollBehavior = scrollBehavior,
+            actions = {
+                IconButton(
+                    onClick = onSignOutClick,
+                    colors = IconButtonDefaults.iconButtonColors(contentColor = MaterialTheme.colorScheme.error)
+                ) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Outlined.Logout,
+                        contentDescription = stringResource(Res.strings.action_sign_out)
+                    )
+                }
+            }
         )
     }
 
